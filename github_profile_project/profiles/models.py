80d8e9b90fae93a0cc121dcc7372526182f4435d
Thembani Maswanganyi <t.maswanganyi101@gmail.com>
2025-03-23 17:04:58 +0200

    Update models.py

--- github_profile_project/profiles/models.py
+++ github_profile_project/profiles/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True, null=True)
+    profile_pic = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+    github_url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+    
+class Repository(models.Model):
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='repositories')
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    repo_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
