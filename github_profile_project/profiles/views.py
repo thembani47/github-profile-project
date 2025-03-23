823350d9d7e5e5e6fc92430befad44680b976862
Thembani Maswanganyi <t.maswanganyi101@gmail.com>
2025-03-23 17:47:22 +0200

    Update views.py

--- github_profile_project/profiles/views.py
+++ github_profile_project/profiles/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile, Repository
 
-# Create your views here.
+def profile(request, username):
+    user_profile = get_object_or_404(UserProfile, username=username)
+    repositories = Repository.objects.filter(user_profile=user_profile).order_by('-stars')
+    return render(request, 'profiles/profile.html', {'user_profile': user_profile, 'repositories': repositories})
